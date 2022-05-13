d71138da0e2dfff052df32a3d7da32ff7aae2533
Cleiton Domazak <cleiton.domazak@farfetch.com>
2022-05-13 18:15:47 +0100

    [FEAT] fixing

--- fonts/install.sh
+++ fonts/install.sh
@@ -1,17 +1,2 @@
 #!/bin/sh
-install() {
-	curl -L -s -o "$1/SourceCodePro-Light.ttf" \
-		https://github.com/adobe-fonts/source-code-pro/raw/release/TTF/SourceCodePro-Light.ttf
-}
-
-if [ "$(uname -s)" = "Darwin" ]; then
-	if command -v brew >/dev/null 2>&1; then
-		brew tap homebrew/cask-fonts
-		brew install font-source-code-pro
-	else
-		install ~/Library/Fonts
-	fi
-else
-	mkdir -p ~/.fonts
-	install ~/.fonts
-fi
+brew install --cask font-inconsolata font-inconsolata-nerd-font
